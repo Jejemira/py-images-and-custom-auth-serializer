2e92dda2b7eddd4a9e32b8a28379aa8528ddfad0
Tkachov Mykhailo <mtkachov435@gmail.com>
2025-10-20 17:29:11 +0300

    Solution2

--- user/admin.py
+++ user/admin.py
@@ -12,13 +12,25 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Personal info", {"fields": ("name",)}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Permissions", {"fields": (
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        )}),
         ("Important dates", {"fields": ("last_login",)}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "password1", "password2", "is_staff", "is_superuser"),
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_superuser"
+            ),
         }),
     )
     search_fields = ("email", "name")
